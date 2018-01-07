c335af2ad2350534263243ed56a5cc3b32b9d7e3
Tom Casey <tqcasey@gmail.com>
2018-01-07 12:46:44 -0500

    add some function skeletons

--- resolve.go
+++ resolve.go
@@ -1,10 +1,37 @@
 package main
 import (
+    "C"
     "os"
     "debug/elf"
+    "debug/dwarf"
 )
+//export address_to_function
+func address_to_function(filename string, addr uint64) string{
+    dwarf_data := get_dwarf_data(string)
+    reader := dwarf_data.Reader()
+    entry, err := reader.SeekPC(addr)
+    if err != nil {
+        return ""
+    }
+    
+}
+//export variable_to_address
+func variable_to_address(filename string, varname string) uint64 {
+    dwarf_data := get_dwarf_data(string)
+    reader := dwarf_data.Reader()
+}
+//export line_to_address
+func line_to_address(string filename, string sourcefile, line_num int) uint64{
+    dwarf_data := get_dwarf_data(string)
+    reader := dwarf_data.Reader()
+}
+//export address_to_line
+func line_to_address(string filename, addr uint64) string {
+    dwarf_data := get_dwarf_data(string)
+    reader := dwarf_data.Reader()
+}
 
-func variable_to_address(filename string, variable_name string, scope uintptr){
+func get_dwarf_data(filename string) DWARF.data {
     r,err := os.Open(filename)
     if err !=nil {
         panic(err)
@@ -17,5 +44,6 @@ func variable_to_address(filename string, variable_name string, scope uintptr){
     if err!=nil{
         panic(err)
     }
-    
+    return dwarf_data
 }
+func main(){}
