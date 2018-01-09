5b1a51ec9acd55e0d6082af0e9d0349ec7fdb4aa
Tom Casey <tqcasey@gmail.com>
2018-01-09 17:15:33 -0500

    add some comments

--- breakpoint.c
+++ breakpoint.c
@@ -3,16 +3,14 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/ptrace.h>
-#include <libexplain/ptrace.h>
+//insert a breakpoint into the program running at PID(must be being ptraced)
 int insert_bp(Breakpoint * bp, pid_t pid) {
+    //save the instruction we're overwriting
     bp->save = ptrace(PTRACE_PEEKDATA, pid, bp->addr, NULL);
     printf("Data at %p is %lx\n", bp->addr, bp->save);
     if (bp->save == -1) {
         printf("PTRACE_PEEKDATA got error %d accessing memory at %p\n", errno,
                bp->addr);
-        const char *explanation =
-            explain_errno_ptrace(errno, PTRACE_PEEKDATA, pid, bp->addr, NULL);
-        printf("Explanation: %s\n", explanation);
     }
     //replace last byte in data with our INT 3(0xcc) instruction
     ptrace(PTRACE_POKEDATA, pid, bp->addr, ((bp->save & ~0xff) | 0xcc));
@@ -20,6 +18,7 @@ int insert_bp(Breakpoint * bp, pid_t pid) {
     bp->enabled = 1;
     return 1;
 }
+//replace the interrupt instruction with the original instruction
 int remove_bp(Breakpoint * bp, pid_t pid) {
     ptrace(PTRACE_POKEDATA, pid, bp->addr, bp->save);
     bp->enabled = 0;
--- tdb.c
+++ tdb.c
@@ -18,6 +18,7 @@ int main(int argc, char *argv[]) {
     pid_t pid = -1;
     int sflag = 0;
     int c;
+    //TODO: allow "-"s in child arguments
     while ((c = getopt(argc, argv, "sb:p:")) != -1) {
         switch (c) {
            case 's':
@@ -35,17 +36,20 @@ int main(int argc, char *argv[]) {
                exit(EXIT_FAILURE);
         }
     }
+    //split comma separated breakpoints into string array
     char **bps = NULL;
     if (brkpt) {
         printf("b argument is %s\n", brkpt);
         bps = str_split(brkpt, ',');
     }
+    //if -p flag isn't used, try to spawn a child
     if (pid == -1) {
         pid = fork_to_child(argc - optind, &argv[optind]);
     }
     if (sflag == 1) {
         ptrace(PTRACE_SINGLESTEP, pid, NULL, 0);
     }
+    //catch the first SIGTRAP upon attaching, we dont need to prompt
     int ss_status;
     waitpid(pid, &ss_status, 0);
     if (!WIFSTOPPED(ss_status)) {
@@ -60,6 +64,7 @@ int main(int argc, char *argv[]) {
         if (num_bpts > 256) {
             fprintf(stderr, "Error: Too many breakpoints");
         }
+        //for each breakpoint requested, create a struct instance for it and insert
         for (int i = 0; i < num_bpts; i++) {
             bplist[i] = malloc(sizeof(Breakpoint));
             sscanf(bps[i], "%x", &bplist[i]->addr);
@@ -73,8 +78,10 @@ int main(int argc, char *argv[]) {
             insert_bp(bplist[i], pid);
         }
     }
+    //now that we have our breakpoints in place we can start running the program
     ptrace(PTRACE_CONT, pid, NULL, 0);
     int status;
+    //some form of a repl loop that waits for a breakpoint/sigtrap to prompt
     while (pid == waitpid(pid, &status, 0)) {
         if (WIFEXITED(status)) {
             printf("Child exited with status %d\n", WEXITSTATUS(status));
@@ -93,8 +100,10 @@ int main(int argc, char *argv[]) {
             char *line = NULL;
             getline(&line, &line_size, stdin);
             char *pos;
+            //take out the \n at the end of the string
             if ((pos = strchr(line, '\n')) != NULL)
                 *pos = '\0';
+            //get our 3 arguments and place them in the proper places
             char *token;
             char *state;
             token = strtok_r(line, " ", &state);
@@ -117,31 +126,40 @@ int main(int argc, char *argv[]) {
             fflush(stdin);
             switch (input) {
                case 'q':
+                   //TODO: free alloc'd memory
                    exit(EXIT_SUCCESS);
                case 's':
                    ptrace(PTRACE_SINGLESTEP, pid, NULL, 0);
                    break;
                case 'c':
-                   asm("nop");  //C is shit and this makes the compiler happy
+                   asm("nop");  //this makes the compiler happy
                    int num_breakpoints =
                        sizeof(bplist) / sizeof(Breakpoint *);
+                   //acquire current register values
                    struct user_regs_struct regs;
                    ptrace(PTRACE_GETREGS, pid, NULL, &regs);
+                   //see if we have any breakpoints enabled that are at pc-1
                    void *location = (void *) regs.rip - 1;
                    for (int i = 0; i < num_breakpoints; i++) {
                        if (bplist[i]->addr == location && bplist[i]->enabled) {
+                           //set the pc back on the instruction we replaced with 0xCC
                            regs.rip = regs.rip - 1;
                            ptrace(PTRACE_SETREGS, pid, NULL, &regs);
+                           //replace the modified instruction with the original
                            remove_bp(bplist[i], pid);
+                           //step over it and catch the SIGTRAP
                            ptrace(PTRACE_SINGLESTEP, pid, NULL, NULL);
                            waitpid(pid, NULL, 0);
+                           //put the breakpoint back in
                            insert_bp(bplist[i], pid);
                            break;
                        }
                    }
+                   //continue on our merry way
                    ptrace(PTRACE_CONT, pid, NULL, 0);
                    break;
                case 'm':
+                   //if we dont have a register to modify, throw a fit
                    if (!opt) {
                        printf
                            ("Error invalid command: m [register] [value]\n");
@@ -152,6 +170,7 @@ int main(int argc, char *argv[]) {
                    }
                    goto redo;
                case 'r':
+                   //if user specified a register, only print that one
                    if (opt) {
                        if (print_register(pid, opt) == -1) {
                            printf("Register not found\n");
@@ -160,12 +179,14 @@ int main(int argc, char *argv[]) {
                    }
                    print_all_registers(pid);
                default:
+                   //loop back
                    goto redo;   //TODO: get rid of this shit somehow
             }
         }
     }
 }
 
+//create child process that gets ptraced
 int fork_to_child(int argc, char *argv[]) {
     pid_t childpid = fork();
     ptrace(PTRACE_SETOPTIONS, childpid, NULL, PTRACE_O_TRACEEXEC);
@@ -178,18 +199,26 @@ int fork_to_child(int argc, char *argv[]) {
     }
     return childpid;
 }
+
+//print the requested register if it exists
 int print_register(pid_t pid, char *reg) {
+    //get all registers
     struct user_regs_struct regs;
     ptrace(PTRACE_GETREGS, pid, NULL, &regs);
+    //request the address of the register in our user_regs_struct
     unsigned long long int *addr = register_to_address(&regs, reg);
+    //check if we got a valid address back
     if (!addr) {
         printf("Register \"%s\" not found\n", reg);
         return -1;
     }
     printf("%s: %llx\n", reg, *addr);
 }
+
+//resolve a string of a register name to its address in a given referenced user_regs_struct
 unsigned long long int *register_to_address(struct user_regs_struct *regs,
                                             char *string) {
+    //ugly if chain(there's no reliable way to avoid this since we don't know exactly how things are aligned in the struct)
     if (strcmp(string, "r15") == 0)
         return &(regs->r15);
     if (strcmp(string, "r14") == 0)
@@ -227,6 +256,8 @@ unsigned long long int *register_to_address(struct user_regs_struct *regs,
     else
         return NULL;
 }
+
+//print all registers
 int print_all_registers(pid_t pid) {
     struct user_regs_struct regs;
     ptrace(PTRACE_GETREGS, pid, NULL, &regs);
@@ -244,10 +275,17 @@ int print_all_registers(pid_t pid) {
     puts("Architecure not supported");
 #endif
 }
+//modify a given register
 int modify_register(pid_t pid, char *reg, unsigned long long int value) {
     struct user_regs_struct regs;
     ptrace(PTRACE_GETREGS, pid, NULL, &regs);
+    //resolve our string to a register
     unsigned long long int *target = register_to_address(&regs, reg);
+    if(!target){
+        printf("Bad register name\n");
+        return -1;
+    }
+    //send the modified version back to the kernel
     (*target) = value;
     ptrace(PTRACE_SETREGS, pid, NULL, &regs);
 }
